9c03adacae1ef225e3cf94168c7caf657ee09c15
JoaoLucasMBC <joaolucasmbcadorniga@gmail.com>
2023-04-10 10:54:11 -0300

    basic structure

---
+++ MoviePredicter.py
@@ -0,0 +1,54 @@
+from scipy.linalg import svd, diagsvd
+
+
+class MoviePredicter():
+
+    def __init__(self, data):
+        self.data = data
+
+
+    def predict(self, i:int, j:int) -> float:
+        u, s, vt = svd(self.data)
+
+        print(s.shape)
+
+        s[-130:] *= 0.0
+
+        sigma = diagsvd(s, self.data.shape[0], self.data.shape[1])
+
+        B = u @ sigma @ vt
+
+        return B[i][j]
+
+
+
+def main():
+    import pandas as pd
+    import numpy as np
+
+    df = pd.read_csv('ratings_small.csv')
+    
+    data = pd.pivot_table(df, values='rating', index='userId', columns='movieId', fill_value=0)
+
+    data = data.to_numpy()
+
+    mp = MoviePredicter(data)
+
+    # Selects a random row and a random column from the data matrix
+    i = np.random.randint(0, data.shape[0])
+    j = np.random.randint(0, data.shape[1])
+
+    real_score = data[i][j]
+
+    # Inserts noise into the data matrix
+    data[i][j] = np.random.randint(0, 5)
+
+    predicted_score = mp.predict(i, j)
+
+    print(f'Real score: {real_score}')
+    print(f'Predicted score: {predicted_score}')
+
+
+
+if __name__ == '__main__':
+    main()
